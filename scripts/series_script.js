82e953daed3236f5a0bea83320e02d315c0dcf13
JesterMoonDesign <82544432+JesterMoonDesign@users.noreply.github.com>
2023-03-27 00:37:41 +0300

    Add files via upload

--- scripts/series_script.js
+++ scripts/series_script.js
@@ -67,6 +67,7 @@ function deleteAnim () {
 
     for (let i = 0; i < sliders.length; i++) {
         let slider = sliders[i];
+        // xData.slider = slider;
         let timeout = setTimeout(() => {
             slider.style.animation = "none";
             slider.style.opacity = "1";
@@ -86,110 +87,110 @@ if(isMobile.any()) {
     mobileIndex = 3;
 };
 
-// function seriesSliders () {
-//     let sliderWrappers = document.getElementsByClassName('sliderWrapper');
-//     const image = document.getElementsByClassName('sliderImageWrapper');
-//     let imageWidth = parseInt(getComputedStyle(image[0]).width);
-//     let xData = {};
-//     let y = mobileIndex;
-//     let x1 = 0;
-//     let x2 = 0;
-//     for(let i = 0; i < sliderWrappers.length; i++) {
+
+
+// let sliderWrappers = document.getElementsByClassName('sliderWrapper');
+// const image = document.getElementsByClassName('sliderImageWrapper');
+// let imageWidth = parseInt(getComputedStyle(image[0]).width);
+// let xData = {};
+// let y = mobileIndex;
+// let slidersLength = {};
+
+
+// function startSlide (event) {
+//     event.preventDefault();
+
+//     xData.sliderWrapper = this;
+//     let slider = this.childNodes[1];
+//     xData.slider = slider;
+
+//     let matrex = window.getComputedStyle(slider).getPropertyValue("transform");
+//     let matrexArr = matrex.split(", ");
+//     xData.x0 = parseInt(matrexArr[4]);
+//     xData.e1 = xData.x0;
+//     console.log('x0_' + xData.x0, 'e1_'+xData.e1)
     
-//         function getX0 () {
-//             let matrex = window.getComputedStyle(sliders[i]).getPropertyValue("transform");
-//             let matrexArr = matrex.split(", ");
-//             xData.x0 = parseInt(matrexArr[4]);
-//         }getX0();
+//     xData.x1 = event.layerX;
+
+//     slidersLength = this.getElementsByClassName('sliderImageWrapper');
+    
+//     this.style.pointerEvents="none";
+//     this.setPointerCapture(event.pointerId);
+//     this.ondragstart = () => false;
+
+//     this.onpointermove = moveSLide;
+//     this.onpointercancel = endSlide;
+//     this.onpointerleave = endSlide;
+// }
+
+// function moveSLide (event) {
+//     x = event.layerX - xData.x1;
+//     xData.slider.style.transform = "translateX(" + x + 'px)';
+// }
+
+// function endSlide () {
+    
+//     let slider = xData.slider;
+
+//     function getX0 () {
+//         let matrex = window.getComputedStyle(slider).getPropertyValue("transform");
+//         let matrexArr = matrex.split(", ");
+//         xData.x0 = parseInt(matrexArr[4]);
+//     }getX0 ();
+//     console.log('x0_' + xData.x0, 'e1_'+xData.e1)
 
 
-//         sliderWrappers[i].onpointerdown = function (event) {
-//             sliderWrappers[i].setPointerCapture(event.pointerId);
-
-//             if (isMobile.any()) {
-//                 let pagePosition = window.scrollY;
-//                 document.body.classList.add('disable-scroll');
-//                 xData.pageX = pagePosition;
-//                 document.body.style.top = -pagePosition + 'px';
-//                 sliderWrappers[i].style.touchAction= "none";
-//                 console.log(pagePosition)
-//             };
-
-
-//             let slidersLength = sliders[i].getElementsByClassName('sliderImageWrapper').length;
-//             let e1 = xData.x0;
-//             let e2 = 0;
-//             x1 = event.offsetX;
-            
-//             sliderWrappers[i].onpointermove = function (event) {
-//                 x2 = event.offsetX;
-//                 x = x2 - x1;
-//                 sliders[i].style.transform = "translateX(" + x + 'px)';
-//                 sliders[i].style.webkitTransform = "translateX(" + x + 'px)';
-//                 sliders[i].style.mozTransform = "translateX(" + x + 'px)';
+//     if (xData.x0 < xData.e1 && xData.x0-xData.e1 < (-0.5*imageWidth) && y < slidersLength) {
+//         if (y>=slidersLength) {
+//             y=slidersLength;
+//         } else if (y<slidersLength) {
+//             y++;
+//             if (xData.x0-xData.e1 < (-1.5*imageWidth)) {
+//                 y++;
+//                 if (xData.x0-xData.e1 < (-2.5*imageWidth)) {
+//                     y++;
+//                 }
 //             }
-
-//             function endSlide () {
-//                 if (isMobile.any()) {
-//                     pagePosition = xData.pageX;
-//                     document.body.style.top = 'auto';
-//                     window.scroll({ top: pagePosition, left: 0 });
-//                     document.body.classList.remove('disable-scroll');
-//                     sliderWrappers[i].style.touchAction = "auto";
-//                     console.log(pagePosition)
+//         };
+//     } else if (y>mobileIndex && xData.x0 > e1 && xData.x0-xData.e1 > (0.5*imageWidth)) {
+//         if (y<=mobileIndex) {
+//             y=mobileIndex;
+//         } else {
+//             y--;
+//             if (xData.x0-xData.e1 > (1.5*imageWidth)) {
+//                 y--;
+//                 if (xData.x0-xData.e1 > (2.5*imageWidth)) {
+//                     y--;
 //                 }
-//                 getX0 ();
-//                 e2 = xData.x0;
-//                 if (e2 < e1 && e2-e1 < (-0.5*imageWidth) && y < slidersLength) {
-//                     if (y>=slidersLength) {
-//                         y=slidersLength;
-//                     } else if (y<slidersLength) {
-//                         y++;
-//                         if (e2-e1 < (-1.5*imageWidth)) {
-//                             y++;
-//                             if (e2-e1 < (-2.5*imageWidth)) {
-//                                 y++;
-//                             }
-//                         }
-//                     };
-//                 } else if (y>mobileIndex && e2 > e1 && e2-e1 > (0.5*imageWidth)) {
-//                     if (y<=mobileIndex) {
-//                         y=mobileIndex;
-//                     } else {
-//                         y--;
-//                         if (e2-e1 > (1.5*imageWidth)) {
-//                             y--;
-//                             if (e2-e1 > (2.5*imageWidth)) {
-//                                 y--;
-//                             }
-//                         }
-//                     }
-//                 };
-//                 if (y>=slidersLength) {
-//                     y=slidersLength;
-//                 }
-//                 if (y<=mobileIndex) {
-//                     y=mobileIndex;
-//                 };
-//                 if (y>mobileIndex && y <= slidersLength) {
-//                     sliders[i].style.transform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
-//                     sliders[i].style.webkitTransform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
-//                     sliders[i].style.mozTransform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
-//                 } else if (y==mobileIndex) {
-//                     sliders[i].style.transform = "translateX(0px)";
-//                     sliders[i].style.webkitTransform = "translateX(0px)";
-//                     sliders[i].style.mozTransform = "translateX(0px)";
-//                 }
-//                 getX0 ();
-//                 sliderWrappers[i].onpointermove = null;
-                
-//             };
-//             sliderWrappers[i].onpointerup = endSlide;
+//             }
 //         }
+//     };
+//     if (y>=slidersLength) {
+//         y=slidersLength;
 //     }
-// }seriesSliders ();
+//     if (y<=mobileIndex) {
+//         y=mobileIndex;
+//     };
+//     if (y>mobileIndex && y <= slidersLength) {
+//         xData.slider.style.transform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
+//         xData.slider.style.webkitTransform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
+//         xData.slider.style.mozTransform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
+//     } else if (y==mobileIndex) {
+//         xData.slider.style.transform = "translateX(0px)";
+//         xData.slider.style.webkitTransform = "translateX(0px)";
+//         xData.slider.style.mozTransform = "translateX(0px)";
+//     }
+//     getX0 ();
+//     console.log('x0_' + xData.x0, 'e1_'+xData.e1)
+//     xData.sliderWrapper.onpointermove = null;
+//     xData.sliderWrapper.onpointerup = null;
+//     xData.sliderWrapper.style.pointerEvents="auto";
+// };
 
-
+// for (const sliderWrapper of sliderWrappers) {
+//     sliderWrapper.onpointerdown = startSlide;
+//     sliderWrapper.onpointerup = endSlide;
+// }
 
 
 function seriesSliders () {
@@ -201,7 +202,6 @@ function seriesSliders () {
     let x1 = 0;
     let x2 = 0;
     for(let i = 0; i < sliderWrappers.length; i++) {
-        let slidersLength = sliders[i].getElementsByClassName('sliderImageWrapper').length;
     
         function getX0 () {
             let matrex = window.getComputedStyle(sliders[i]).getPropertyValue("transform");
@@ -209,100 +209,97 @@ function seriesSliders () {
             xData.x0 = parseInt(matrexArr[4]);
         }getX0();
 
-        function checkMobile () {
+
+        sliderWrappers[i].onpointerdown = function (event) {
+            event.preventDefault();
+            sliderWrappers[i].setPointerCapture(event.pointerId);
+
             if (isMobile.any()) {
                 let pagePosition = window.scrollY;
                 document.body.classList.add('disable-scroll');
                 xData.pageX = pagePosition;
                 document.body.style.top = -pagePosition + 'px';
-                sliderWrappers[i].style.touchAction= "none";
+                sliders[i].style.pointerEvents="none";
             };
-        }
 
-        function startSlide (event) {
-            document.body.classList.add('disable-scroll');
-            event.preventDefault();
-            event.stopPropagation();
-            checkMobile ();
-        
-            sliderWrappers[i].setPointerCapture(event.pointerId);
-            
-            xData.x1 = event.offsetX;
-            
-            sliderWrappers[i].onpointermove = moveSlide;
-        
-            
-            sliderWrappers[i].onpointerup = endSlide;
-        }
-        
-        function endSlide () {
-            if (isMobile.any()) {
-                pagePosition = xData.pageX;
-                document.body.style.top = 'auto';
-                window.scroll({ top: pagePosition, left: 0 });
-                document.body.classList.remove('disable-scroll');
-                sliderWrappers[i].style.touchAction = "auto";
-                console.log(pagePosition)
-            }
+
+            let slidersLength = sliders[i].getElementsByClassName('sliderImageWrapper').length;
             let e1 = xData.x0;
-            getX0 ();
-            let e2 = xData.x0;
-            if (e2 < e1 && e2-e1 < (-0.5*imageWidth) && y < slidersLength) {
-                if (y>=slidersLength) {
-                    y=slidersLength;
-                } else if (y<slidersLength) {
-                    y++;
-                    if (e2-e1 < (-1.5*imageWidth)) {
+            let e2 = 0;
+            x1 = event.offsetX;
+            
+            sliderWrappers[i].onpointermove = function (event) {
+                x2 = event.offsetX;
+                x = x2 - x1;
+                sliders[i].style.transform = "translateX(" + x + 'px)';
+                sliders[i].style.webkitTransform = "translateX(" + x + 'px)';
+                sliders[i].style.mozTransform = "translateX(" + x + 'px)';
+            }
+
+            function endSlide () {
+                getX0 ();
+                e2 = xData.x0;
+                if (e2 < e1 && e2-e1 < (-0.5*imageWidth) && y < slidersLength) {
+                    if (y>=slidersLength) {
+                        y=slidersLength;
+                    } else if (y<slidersLength) {
                         y++;
-                        if (e2-e1 < (-2.5*imageWidth)) {
+                        if (e2-e1 < (-1.5*imageWidth)) {
                             y++;
+                            if (e2-e1 < (-2.5*imageWidth)) {
+                                y++;
+                            }
+                        }
+                    };
+                } else if (y>mobileIndex && e2 > e1 && e2-e1 > (0.5*imageWidth)) {
+                    if (y<=mobileIndex) {
+                        y=mobileIndex;
+                    } else {
+                        y--;
+                        if (e2-e1 > (1.5*imageWidth)) {
+                            y--;
+                            if (e2-e1 > (2.5*imageWidth)) {
+                                y--;
+                            }
                         }
                     }
                 };
-            } else if (y>mobileIndex && e2 > e1 && e2-e1 > (0.5*imageWidth)) {
+                if (y>=slidersLength) {
+                    y=slidersLength;
+                }
                 if (y<=mobileIndex) {
                     y=mobileIndex;
-                } else {
-                    y--;
-                    if (e2-e1 > (1.5*imageWidth)) {
-                        y--;
-                        if (e2-e1 > (2.5*imageWidth)) {
-                            y--;
-                        }
-                    }
+                };
+                if (y>mobileIndex && y <= slidersLength) {
+                    sliders[i].style.transform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
+                    sliders[i].style.webkitTransform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
+                    sliders[i].style.mozTransform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
+                } else if (y==mobileIndex) {
+                    sliders[i].style.transform = "translateX(0px)";
+                    sliders[i].style.webkitTransform = "translateX(0px)";
+                    sliders[i].style.mozTransform = "translateX(0px)";
                 }
+                getX0 ();
+                if (isMobile.any()) {
+                    pagePosition = xData.pageX;
+                    document.body.style.top = '';
+                    window.scroll({ top: pagePosition, left: 0 });
+                    document.body.classList.remove('disable-scroll');
+                    sliders[i].style.pointerEvents="auto";
+                }
+                sliderWrappers[i].onpointermove = null;
+                sliderWrappers[i].onpointerup = null;
+                sliderWrappers[i].onpointercancel = null;
+                sliderWrappers[i].onpointerleave = null;
+                
             };
-            if (y>=slidersLength) {
-                y=slidersLength;
-            }
-            if (y<=mobileIndex) {
-                y=mobileIndex;
-            };
-            if (y>mobileIndex && y <= slidersLength) {
-                sliders[i].style.transform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
-                sliders[i].style.webkitTransform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
-                sliders[i].style.mozTransform = "translateX(" + ((y-mobileIndex)*-imageWidth-(y-mobileIndex)*20) + "px)";
-            } else if (y==mobileIndex) {
-                sliders[i].style.transform = "translateX(0px)";
-                sliders[i].style.webkitTransform = "translateX(0px)";
-                sliders[i].style.mozTransform = "translateX(0px)";
-            }
-            getX0 ();
-            sliderWrappers[i].onpointermove = null;
-            sliderWrappers[i].onpointerup = null;
-            
-        };
-        
-        function moveSlide (event) {
-            x2 = event.offsetX;
-            x = x2 - xData.x1;
-            sliders[i].style.transform = "translateX(" + x + 'px)';
-            sliders[i].style.webkitTransform = "translateX(" + x + 'px)';
-            sliders[i].style.mozTransform = "translateX(" + x + 'px)';
+            sliderWrappers[i].onpointerup = endSlide;
+            sliderWrappers[i].onpointercancel = endSlide;
+            sliderWrappers[i].onpointerleave = endSlide;
+            sliderWrappers[i].onpointerleave = console.log('leave');
+            sliderWrappers[i].onpointercancel = console.log('cancel');
         }
-        
-        sliderWrappers[i].onpointerdown = startSlide;
-        sliderWrappers[i].ondragstart = () => false;        
-        
     }
 }seriesSliders ();
+
+
